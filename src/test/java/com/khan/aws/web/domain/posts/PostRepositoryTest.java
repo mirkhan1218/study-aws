a9c7b20791bc301a113b25540dd42eab15531eac
mirkhan <mirkhan1218@gmail.com>
2023-08-21 16:07:44 +0900

    PostsRepository 테스트 코드 구현

---
+++ src/test/java/com/khan/aws/web/domain/posts/PostRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.khan.aws.web.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest // h2 데이터 베이스 자동 실행
+class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("test@gmail.com")
+                .build());
+
+        // when
+        List<Posts> postsList = postRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
